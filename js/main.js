c6545cec5a4d9f9c122f8f439597c0a5e620ba83
rahil ahmadi <67064503+rahilAhmadi@users.noreply.github.com>
2023-08-05 23:55:30 +0330

    add main.js file to add js codes and navbar change js code

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let header_menu=document.querySelector('.menu_header');
+console.log(pageYOffset)
+window.addEventListener('scroll',()=>{
+    if(pageYOffset>50){
+        header_menu.classList.add('sticky');
+    }
+    else{
+        header_menu.classList.remove('sticky');
+    }
+    if(pageYOffset>568){
+        header_menu.classList.add('black_them');
+    }
+    else{
+        header_menu.classList.remove('black_them');
+    }
+})
